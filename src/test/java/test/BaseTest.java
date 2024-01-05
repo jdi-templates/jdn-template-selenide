fb57bb01caad23007c0fa622aed3fb73521af230
Aleksandr_Vorobev <aleksandr.vorobev@tvh.com>
2024-01-05 15:36:16 +0300

    update pom file

--- src/test/java/test/BaseTest.java
+++ src/test/java/test/BaseTest.java
@@ -2,6 +2,8 @@ package test;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Listeners;
@@ -12,6 +14,8 @@ import static com.codeborne.selenide.FileDownloadMode.FOLDER;
 public interface BaseTest {
     @BeforeSuite
     public static void setUp() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide().screenshots(true)
+                .savePageSource(false));
         Configuration.baseUrl = "https://www.example.com";
         Configuration.browser = "chrome"; // You can also use "firefox", "edge", etc.
         Configuration.pageLoadTimeout = 20000;
@@ -23,7 +27,7 @@ public interface BaseTest {
 
     @AfterSuite
     public static void tearDown() {
-        // Close all open browsers
+        SelenideLogger.removeListener("AllureSelenide");
         Selenide.closeWebDriver();
     }
 }
--- src/test/java/test/ExampleTests.java
+++ src/test/java/test/ExampleTests.java
@@ -1,5 +1,6 @@
 package test;
 
+import io.qameta.allure.Description;
 import org.testng.annotations.Test;
 import site.pages.HomePage;
 
@@ -7,6 +8,7 @@ public class ExampleTests implements BaseTest {
     HomePage homePage = new HomePage();
 
     @Test
+    @Description("Example Test")
     public void openPageTest() {
         homePage.openPage();
         homePage.moreInformationLink.click();
