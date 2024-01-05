9bd049f18a19c1a4e0b33a534fc48620c3bf402b
Aleksandr_Vorobev <aleksandr.vorobev@tvh.com>
2024-01-05 15:37:59 +0300

    set element name

--- src/main/java/site/pages/HomePage.java
+++ src/main/java/site/pages/HomePage.java
@@ -7,7 +7,7 @@ import static com.codeborne.selenide.Selenide.$;
 public class HomePage extends BasePage {
     private static final String PATH = "/";
     private static final String TITLE = "/";
-    public SelenideElement moreInformationLink = $("p > a");
+    public SelenideElement moreInformationLink = $("p > a").as("more information link");
 
     public HomePage() {
         super(TITLE, PATH);
