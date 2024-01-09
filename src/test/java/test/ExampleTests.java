21d66b60c2f8efe7ab3fd7bb65e25a4ab05c8bd1
Aleksandr_Vorobev <aleksandr.vorobev@tvh.com>
2024-01-09 15:57:03 +0300

    downgrade selenide to support java 11

--- src/test/java/test/ExampleTests.java
+++ src/test/java/test/ExampleTests.java
@@ -1,14 +1,12 @@
 package test;
 
-import io.qameta.allure.Description;
 import org.testng.annotations.Test;
 import site.pages.HomePage;
 
 public class ExampleTests implements BaseTest {
     HomePage homePage = new HomePage();
 
-    @Test
-    @Description("Example Test")
+    @Test(description = "Example test")
     public void openPageTest() {
         homePage.openPage();
         homePage.moreInformationLink.click();
